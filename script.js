0a5c485ae2bc48da6b17687cde1012452957c5e6
Gabriel-Daantas <gabrieldb2003@gmail.com>
2024-03-11 21:47:51 -0300

    Inicio dos scripts para adicionar documentos

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const helpButton = document.querySelector('.help-button');
     const helpBox = document.querySelector('.help-box');
     const logoNavbar = document.querySelector('.logo-navbar');
+    const archiveBox = document.querySelector('.archive-box');
 
     menuBar.addEventListener('click', function (event) {
         // Obtendo as coordenadas do clique.
@@ -20,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function () {
             clickY >= pseudoRect.top &&
             clickY <= pseudoRect.top + parseFloat(pseudoStyles.height)
         ) {
-            menuBar.classList.toggle('close-menu');
+            menuBar.classList.toggle('open-menu');
             logoNavbar.classList.toggle('opacity-0');
         }
     });
@@ -31,4 +32,27 @@ document.addEventListener('DOMContentLoaded', function () {
         helpButton.classList.toggle('help-button-hover');
     })
 
+
+    archiveBox.addEventListener('click', function () {
+        document.getElementById('fileInput').click();
+    });
+
+    archiveBox.addEventListener('dragover', function (event) {
+        event.preventDefault();
+    });
+
+    archiveBox.addEventListener('drop', function (event) {
+        event.preventDefault();
+        var files = event.dataTransfer.files;
+        handleFiles(files);
+    });
+
+
+    function handleFiles(files) {
+        
+        for (var i = 0; i < files.length; i++) {
+            alert("Nome do arquivo: " + files[i].name);
+            
+        }
+    }
 });
